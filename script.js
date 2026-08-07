23ef1de9f165c14559a326e5606bacd2a23ccd7e
Edwin Estevez <edwinestevez31@gmail.com>
2026-08-07 15:56:45 -0400

    Store message components complete

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("My message generator is Workign!");
+const beginnings = [
+    "Today you will",
+    "Tomorrow you will",
+    "This week you will"
+];
+
+const events = [
+    "learn something new",
+    "receive good news",
+    "meet someone interesting"
+];
+
+const endings = [
+    "Trust your instincts.",
+    "Your lucky number is 7.",
+    "Keep going!"
+];
